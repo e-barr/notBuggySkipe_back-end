41826bec0c72053f7fa5d15ccff68e6d2c309781
e-barr <github email address>
2018-10-10 16:57:16 -0500

    successfully seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ user1 = User.create(email: "user1@email.com", username: "user1", password: "pass
 user2 = User.create(email: "user2@email.com", username: "user2", password: "password2", city: "San Francisco", country: "USA", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/768px-User_icon_2.svg.png")
 user3 = User.create(email: "user3@email.com", username: "user3", password: "password3", city: "Chicago", country: "USA", image_url: "https://cdn2.iconfinder.com/data/icons/rcons-users-color/32/girl-512.png")
 
-# inbox1 = Inbox.create(user_id: user1.id)
-# inbox2 = Inbox.create(user_id: user2.id)
-# inbox3 = Inbox.create(user_id: user3.id)
-#
-# mr_1 = MeetingRoom.create(name: "room1")
-# mr_2 = MeetingRoom.create(name: "room2")
-# mr_3 = MeetingRoom.create(name: "room3")
-#
-# email1and2 = Email.create(sender_id: user1.id, receiver_id: user2.id, invite: true, room_id: mr_1.id, content: 'Meeting at 2pm in "room1"')
-# email2and3 = Email.create(sender_id: user2.id, receiver_id: user3.id, invite: false, content: 'Greetings')
-# email3and1 = Email.create(sender_id: user3.id, receiver_id: user1.id, invite: false, content: 'Let\'s get cookies')
+inbox1 = Inbox.create(user_id: user1.id)
+inbox2 = Inbox.create(user_id: user2.id)
+inbox3 = Inbox.create(user_id: user3.id)
+
+mr_1 = MeetingRoom.create(name: "room1")
+mr_2 = MeetingRoom.create(name: "room2")
+mr_3 = MeetingRoom.create(name: "room3")
+
+email1and2 = Email.create(sender_id: user1.id, receiver_id: user2.id, invite: true, room_id: mr_1.id, content: 'Meeting at 2pm in "room1"')
+email2and3 = Email.create(sender_id: user2.id, receiver_id: user3.id, invite: false, content: 'Greetings')
+email3and1 = Email.create(sender_id: user3.id, receiver_id: user1.id, invite: false, content: 'Let\'s get cookies')
