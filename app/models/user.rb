b6b5898a8d466832096102eb13b38c15d3904d4e
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-11 15:20:35 -0500

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sent_emails, foreign_key: :sender_id, class_name: :Email
   has_many :received_emails, foreign_key: :receiver_id, class_name: :Email
-
   has_many :contacts, foreign_key: :user_1, class_name: :Contact
+
+  validates :email, :username, uniqueness: true
+  validates :city, :country, :image_url, presence: true
 
   def self.current_users
     User.all.select { |user| user.logged_in == true }
