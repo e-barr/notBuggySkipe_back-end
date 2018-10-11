156a5893a2791a84bccd7b6ed2821eab4a5eae4e
e-barr <github email address>
2018-10-11 10:35:33 -0500

    update contact migration

--- db/migrate/20181011152719_create_contacts.rb
+++ db/migrate/20181011152719_create_contacts.rb
@@ -1,8 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-      t.integer :user_1
-      t.integer :user_2
+      t.integer :user_1_id
+      t.integer :user_2_id
 
       t.timestamps
     end
