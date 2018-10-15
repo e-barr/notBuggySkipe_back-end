4633f8da83b1a8bd4423e4df1f03e2b2f9445b84
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-15 11:12:45 -0500

    update invite

--- app/serializers/invite_serializer.rb
+++ app/serializers/invite_serializer.rb
@@ -1,15 +1,3 @@
 class InviteSerializer < ActiveModel::Serializer
-  attributes :id, :sender_name, :receiver_name, :room_name
-
-  def sender_username
-    sender.username
-  end
-
-  def receiver_username
-    receiver.username
-  end
-
-  def room_name
-    room.name
-  end
+  attributes :id, :sender, :receiver, :room
 end
