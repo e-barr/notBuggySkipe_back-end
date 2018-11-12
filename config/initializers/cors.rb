883ce6fdd61996e2cbcbf51d2c16fb81282546cc
e-barr <31676765+e-barr@users.noreply.github.com>
2018-11-12 15:37:32 -0600

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://e-barr.github.io/*'
+    origins 'https://e-barr.github.io/'
 
     resource '*',
       headers: :any,
